ca9d3ea4c0db8fcc5b6a0f9874a61eb94921824a
Tobias Binna <tobias.binna@toolsplus.io>
2024-03-05 11:07:54 +0800

    feat(nx): update to Nx 18
    BREAKING-CHANGE: requires Nx 18
    
    Closes #66

--- packages/nx-forge/src/generators/application/lib/normalize-options.ts
+++ packages/nx-forge/src/generators/application/lib/normalize-options.ts
@@ -1,5 +1,5 @@
 import { Linter } from '@nx/eslint';
-import type { Tree } from '@nx/devkit';
+import { Tree } from '@nx/devkit';
 import type { ApplicationGeneratorOptions, NormalizedOptions } from '../schema';
 import { determineProjectNameAndRootOptions } from '@nx/devkit/src/generators/project-name-and-root-utils';
 
@@ -26,7 +26,14 @@ export async function normalizeOptions(
     ? options.tags.split(',').map((s) => s.trim())
     : [];
 
+  const addPlugin = process.env.NX_ADD_PLUGINS !== 'false';
+  // At the time we implemented this, 'useInferencePlugins' was not yet available in the NxJsonConfiguration definition.
+  // Add this check once 'useInferencePlugins' is available.
+  // Refer to the check here:https://github.com/nrwl/nx/blob/47df7f94aff3e08314659051817b9c8a8023ac94/packages/node/src/generators/application/application.ts#L550
+  // && nxJson.useInferencePlugins !== false;
+
   return {
+    addPlugin,
     ...options,
     name: appProjectName,
     appProjectRoot,
