1cf9b693cf1f9337bd3df7ffe4375d90770f7440
Tobias Binna <tobias.binna@toolsplus.io>
2024-07-03 17:15:35 +0800

    feat(executors): support bitbucket product option
    update the install executor to support the bitbucket product option
    
    Closes #123

--- packages/nx-forge/src/executors/install/schema.d.ts
+++ packages/nx-forge/src/executors/install/schema.d.ts
@@ -1,7 +1,7 @@
 export interface InstallExecutorOptions {
   outputPath: string;
   site: string;
-  product: 'jira' | 'confluence' | 'compass';
+  product: 'jira' | 'confluence' | 'compass' | 'bitbucket';
   environment: string;
   upgrade: boolean;
   confirmScopes: boolean;
